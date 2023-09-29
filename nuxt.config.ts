fad28146205997f1c239822cdb8be7f7845c7ead
chaitanyaarige <chaitanyaarige@gmail.com>
2023-09-29 07:20:19 +0530

    initial setup pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devServer: {
-    port: 3001,
+    port: 3000,
   },
   components: true,
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode'
+  ],
+  colorMode: {
+    classSuffix: ''
+  },
+
+
   devtools: { enabled: true },
 });
